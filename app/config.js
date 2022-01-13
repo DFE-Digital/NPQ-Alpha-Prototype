45b350d255d371dcf576ac428e33b3eb573a6326
Zabe <me@zabeaziz.com>
2022-01-13 10:47:05 +0000

    Updates

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage training for early career teachers',
+  serviceName: 'Manage CPD contracts',
 
   // Default port that prototype runs on
   port: '3000',
