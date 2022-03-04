3b6f951dc71440c180acbca4e1040a1d1f1d2ec1
Zabe <me@zabeaziz.com>
2022-03-04 16:46:28 +0000

    Version-22 & new page for changing cutoff dates

---
+++ app/views/version-22/routes.js
@@ -0,0 +1,62 @@
+module.exports = (app) => {
+
+    app.get('/prototype/test', function(req, res) {
+        res.write("I am a new route")
+        res.end();
+    });
+  
+  var version = 'version-22'
+   
+    app.post('/' + version + '/payments/payment-breakdown', function (req, res) {
+  
+  
+      if (req.body.scheme === "Early Career Framework (ECF)") {
+          res.redirect('/' + version + '/payments/ecf-payment-breakdown');
+  
+      } else  {
+          res.redirect('/' + version + '/payments/npq-payment-breakdown');
+  
+      }
+      
+        
+    });
+    
+      
+      app.post('/' + version + '/providers/trainee-scheme-route', function (req, res) {
+  
+  
+        if (req.body.scheme === "Early Career Framework (ECF)") {
+            res.redirect('/' + version + '/providers/ecf-payment-breakdown');
+    
+        } else  {
+            res.redirect('/' + version + '/providers/npq-payment-breakdown');
+    
+        }
+        
+    
+      
+    });
+
+  app.post('/' + version + '/user-flow', function (req, res) {
+  
+  
+    if (req.body.user === "cm") {
+        res.redirect('/' + version + '/payments/service-performance');
+
+    } else  {
+        res.redirect('/' + version + '/providers/email-notification');
+
+    }
+    
+
+  
+});
+  
+  
+  
+  }
+  
+   
+  
+  
+  
