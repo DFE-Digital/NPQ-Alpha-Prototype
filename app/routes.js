ad20a69cc6481f465eb8648e93cb973eda504ab6
dchmrc <darren.courtney@digital.education.gov.uk>
2022-11-15 17:02:51 +0000

    Version 24
    - New index page
    - New content
    - Archived v23 (still accessible from new index page)

--- app/routes.js
+++ app/routes.js
@@ -4947,11 +4947,84 @@ router.get('/*/sandbox/ecfchoosy' , function (req, res) {
         }
 })
 
+// VERSION 24
+// ----
+// Run this code when a form is submitted to 'choose-provider-answer'
+router.post('/choose-provider-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'choose-provider'
+  var chooseProvider = req.session.data['choose-provider']
+
+  // Check whether the variable matches a condition
+  if (chooseProvider == "ambition institute"){
+    // Send user to next page
+    res.redirect('/version-24/financial-statement-NPQ-v9')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/version-24/financial-statement-NPQ-v9-page-unavailable')
+  }
+
+})
+
+// Run this code when a form is submitted to 'choose-programme-answer'
+router.post('/choose-programme-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'ecf-or-npq'
+  var chooseProgramme = req.session.data['ecf-or-npq']
+
+  // Check whether the variable matches a condition
+  if (chooseProgramme == "ecf payments"){
+    // Send user to next page
+    res.redirect('/version-24/choose-provider-ecf')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/version-24/choose-provider-npq')
+  }
+
+})
+
+// Run this code when a form is submitted to 'add-adjustment-answer'
+router.post('/add-adjustment-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'add-another'
+  var needAnotherAdjustment = req.session.data['add-another']
+
+  // Check whether the variable matches a condition
+  if (needAnotherAdjustment == "no"){
+    // Send user to next page
+    // change link back to ECF financial statement when design approved
+    res.redirect('/version-24/choose-provider-ecf')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/version-24/add-adjustment-type')
+  }
+
+})
+
+// Run this code when a form is submitted to 'remove-adjustment-answer'
+router.post('/remove-adjustment-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'remove-adjustment'
+  var removeAdjustment = req.session.data['remove-adjustment']
+
+  // Check whether the variable matches a condition
+  if (removeAdjustment == "no"){
+    // Send user to next page
+    res.redirect('/adjustments-add-to-a-list')
+  } else {
+    // Send user to ineligible page
+    // change link back to ECF financial statement when design approved
+    res.redirect('/version-24/choose-provider-ecf')
+  }
+
+})
+
 require('./views/version-18/routes.js')(router);
 require('./views/version-19/routes.js')(router);
 require('./views/version-20/routes.js')(router);
 require('./views/version-21/routes.js')(router);
 require('./views/version-22/routes.js')(router);
 require('./views/version-23/routes.js')(router);
+
 
 module.exports = router
